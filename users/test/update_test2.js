4317199e438b4a1f49c3e8232a34252295373e48
of3jlmssoo <7a4wiy5vd638@gmail.com>
2024-08-01 00:44:53 +0900

    findOneAndUpdate and findOne and assert

---
+++ users/test/update_test2.js
@@ -0,0 +1,42 @@
+// update() deprecated
+// use updateOne or updateMany
+
+const assert = require("assert");
+const User = require("../src/user");
+// const { default: mongoose } = require("mongoose");
+
+describe("Updating records", () => {
+  let joe;
+
+  beforeEach((done) => {
+    joe = new User({ name: "Joe", postCount: 0 });
+    joe.save().then(() => done());
+  });
+
+  function assertName(operation, done) {
+    operation
+      .then(() => User.find({}))
+      .then((users) => {
+        // console.log(users);
+        assert(users.length === 1);
+        assert(users[0].name === "Alex");
+        done();
+      });
+  }
+
+
+  it.only('promise test', () =>
+    User.findOneAndUpdate({name:'Joe'},{postCount:7})
+    .then(() =>
+      User.findOne({ name:'Joe' })
+    )
+    .then(user => {
+      // console.log(user);
+      assert.equal(user.postCount, 7)
+      // return user.destroy() // <- このPromiseが戻り値になる
+    })
+  )  
+
+
+
+});
