feb8474bf54b81e300e9ef58c98f54992da6b17c
of3jlmssoo <7a4wiy5vd638@gmail.com>
2024-07-31 16:17:10 +0900

    39 update by model class methods

---
+++ users/test/update_test.js
@@ -0,0 +1,46 @@
+// update() deprecated
+// use updateOne or updateMany
+
+const assert = require("assert");
+const User = require("../src/user");
+
+describe("Updating records", () => {
+  let joe;
+
+  beforeEach((done) => {
+    joe = new User({ name: "Joe" });
+    joe.save().then(() => done());
+  });
+
+  function assertName(operation, done) {
+    operation
+      .then(() => User.find({}))
+      .then((users) => {
+        // console.log(users);
+        assert(users.length === 1);
+        assert(users[0].name === "Alex");
+        done();
+      });
+  }
+
+  it("instance type using set n save", (done) => {
+    joe.set("name", "Alex");
+    assertName(joe.save(), done);
+  });
+
+  it("model instance can update", (done) => {
+    assertName(joe.updateOne({ name: "Alex" }), done);
+  });
+
+  it("A model class can update", (done) => {
+    assertName(User.updateMany({ name: "Joe" }, { name: "Alex" }), done);
+  });
+
+  it("A model class can update one record", (done) => {
+    assertName(User.findOneAndUpdate({ name: "Joe" }, { name: "Alex" }), done);
+  });
+
+  it("A model class can find a record with an Id and update", (done) => {
+    assertName(User.findByIdAndUpdate(joe._id, { name: "Alex" }), done);
+  });
+});
