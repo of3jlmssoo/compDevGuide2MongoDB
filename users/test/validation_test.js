3497fa404f4d29b460da9140bf66d67cfdd2e8d8
of3jlmssoo <7a4wiy5vd638@gmail.com>
2024-08-01 10:31:30 +0900

    46 unable to save invalid records

--- users/test/validation_test.js
+++ users/test/validation_test.js
@@ -15,11 +15,22 @@ describe("Validating Records", () => {
     assert(message === "name is required.");
   });
 
-  it.only("user name length check", () => {
+  it("user name length check", () => {
     const user = new User({ name: "Jo" });
 
     const validationResult = user.validateSync();
     const { message } = validationResult.errors.name;
-    assert(message === 'Name must be longer than 2 characters.');
+    assert(message === "Name must be longer than 2 characters.");
+  });
+
+  it.only("unable to save invalid records", (done) => {
+    const user = new User({ name: "Jo" });
+    user.save().catch((validationResult) => {
+      // console.log("==> ", validationResult.errors.name.message);
+      const { message } = validationResult.errors.name;
+      // console.log("==> ", message);
+      assert(message === "Name must be longer than 2 characters.");
+      done();
+    });
   });
 });
