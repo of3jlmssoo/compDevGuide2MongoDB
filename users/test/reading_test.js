ae1824bc4c5bd97f74f92c343e50a89ba823abf6
of3jlmssoo <7a4wiy5vd638@gmail.com>
2024-08-07 14:16:39 +0900

    73 sort skip limit

--- users/test/reading_test.js
+++ users/test/reading_test.js
@@ -2,13 +2,19 @@ const assert = require("assert");
 const User = require("../src/user");
 
 describe("Reading users out of the database", () => {
-  let joe;
+  // let joe;
 
+  let alex, joe, maria, zack;
   beforeEach((done) => {
+    // joe = new User({ name: "Joe" });
+    // joe.save().then(() => {
+    //   done();
+    // });
+    alex = new User({ name: "Alex" });
     joe = new User({ name: "Joe" });
-    joe.save().then(() => {
-      done();
-    });
+    maria = new User({ name: "Maria" });
+    zack = new User({ name: "Zack" });
+    Promise.all([alex.save(), joe.save(), maria.save(), zack.save()]).then(() => done());
   });
 
   it("finds all users with a name of joe", (done) => {
@@ -23,5 +29,24 @@ describe("Reading users out of the database", () => {
       assert(user.name === "Joe");
       done();
     });
+  });
+
+  it.only("can skip and limit the result set", (done) => {
+    // User.find({}).then((users) => {
+    //   console.log("can skip? users ", users.length);
+    // });
+
+    // 
+    User.find({})
+      .sort({name:1})
+      .skip(1)
+      .limit(2)
+      .then((users) => {
+        console.log("can skip? users ", users.length);
+        assert(users.length===2);
+        assert(users[0].name==="Joe");
+        assert(users[1].name==="Maria");
+        done();
+      });
   });
 });
